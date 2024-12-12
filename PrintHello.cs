8fadf92b52a142cc7164eb06c3083a1eb73dd216
Arvind1709 <arvindyadav1709@gmail.com>
2024-12-12 21:57:09 +0530

    PrintName is added

---
+++ PrintHello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProjectLearing
+{
+    public class PrintHello
+    {
+        public void PrintNme()
+        {
+            Console.WriteLine("Hello, Arvind's World!");
+        }
+
+    }
+}
