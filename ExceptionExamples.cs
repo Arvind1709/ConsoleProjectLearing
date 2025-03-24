c6d63bca5465a5a22a23222ff662d991e974111a
Arvind1709 <arvindyadav1709@gmail.com>
2025-03-24 21:03:09 +0530

    Last ExceptionException Examples commit don on 24 March 2025

---
+++ ExceptionExamples.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProjectLearing
+{
+    public class ExceptionExamples : Exception
+    {
+        public string devideByZero()
+        {
+           // try
+            //{
+                int a = 10;
+                int b = 0;
+                if (b == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                else
+                {
+                    int c = a / b;
+                }
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    return ex.Message;
+            //    Console.WriteLine("Divide by zero exception");
+            //}
+            //catch (Exception ex)
+            //{
+            //    return ex.Message;
+            //    Console.WriteLine("General exception");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finally block");
+            //}
+            return "End of method";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,13 +7,24 @@ using ConsoleProjectLearing;
 
 Console.WriteLine("Main method starts.");
 
-string name = null;
-string result = name ?? "Default Name";
-Console.WriteLine(result); // Output: Default Name
+//string name = null;
+//string result = name ?? "Default Name";
+//Console.WriteLine(result); // Output: Default Name
 
-string name1 = "Arvind";
-string result1 = name1 ?? "Default Name";
-Console.WriteLine(result1); // Output: Default Name
+//string name1 = "Arvind";
+//string result1 = name1 ?? "Default Name";
+//Console.WriteLine(result1); // Output: Default Name
 
+try
+{
+    ExceptionExamples exceptionExamples = new ExceptionExamples();
+    string result = exceptionExamples.devideByZero();
+    Console.WriteLine(result);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("General exception");
 
-Console.WriteLine("Main method ends.");
+}
+
+    Console.WriteLine("Main method ends.");
